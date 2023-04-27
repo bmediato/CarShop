115cf8946192824c712a5ee879fb700265f959cb
Beatriz <beatriz.mediatto2@gmail.com>
2023-04-27 14:35:52 -0300

    test: rota /cars

--- tests/unit/Services/car.test.ts
+++ tests/unit/Services/car.test.ts
@@ -19,34 +19,31 @@ describe('Rota /cars', function () {
 
   const newCar: Car = new Car(carInput);
 
-  describe('Se é possível cadastrar um carro', function () {
-    it('Cadastrando um carro com sucesso', async function () {
-      sinon.stub(Model, 'create').resolves(newCar);
+  it('Cadastrando um carro com sucesso', async function () {
+    sinon.stub(Model, 'create').resolves(newCar);
 
-      const service = new CarService();
-      const result = await service.createCar(carInput);
+    const service = new CarService();
+    const result = await service.createCar(carInput);
 
-      expect(result).to.be.deep.equal(newCar);
-    });
+    expect(result).to.be.deep.equal(newCar);
   });
-  describe('Se é possível listar os carros', function () {
-    it('Retorna lista completa de carros com sucesso', async function () {
-      sinon.stub(Model, 'find').resolves([newCar]);
 
-      const service = new CarService();
-      const result = await service.getAll();
-      expect(result).to.be.deep.equal([newCar]);
-    });
-    it('Retorna com sucesso apenas o carro com id presente na URL', async function () {
-      sinon.stub(Model, 'findById').resolves(newCar);
+  it('Retorna lista completa de carros com sucesso', async function () {
+    sinon.stub(Model, 'find').resolves([newCar]);
 
-      const service = new CarService();
-      const result = await service.getById('1');
-      expect(result).to.be.deep.equal(newCar);
-    });
-    it('Retorna erro caso id passado não exista no banco', async function () {
-      sinon.stub(Model, 'findById');
-    });
+    const service = new CarService();
+    const result = await service.getAll();
+    expect(result).to.be.deep.equal([newCar]);
+  });
+  it('Retorna com sucesso apenas o carro com id presente na URL', async function () {
+    sinon.stub(Model, 'findById').resolves(newCar);
+
+    const service = new CarService();
+    const result = await service.getById('1');
+    expect(result).to.be.deep.equal(newCar);
+  });
+  it('Retorna erro caso id passado não exista no banco', async function () {
+    sinon.stub(Model, 'findById');
   });
   afterEach(function () {
     sinon.restore();
